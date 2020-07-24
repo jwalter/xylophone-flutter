119202432024baf477dbd94780a848285cb9bf90
Joakim Waltersson <joakim.waltersson@gmail.com>
2020-07-24 16:37:26 +0200

    Add title and app bar

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,11 @@ class XylophoneApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Xylophone',
       home: Scaffold(
+        appBar: AppBar(
+          title: Text('Xylophone'),
+        ),
         body: SafeArea(
           child: Container(),
         ),

f3f84a061dd5cd3373e47aa146a84b5b63d793ba
Joakim Waltersson <Joakim.waltersson@gmail.com>
2020-07-25 19:57:55 +0200

    Add audio playing support

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,45 @@
+import 'package:audioplayers/audio_cache.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(XylophoneApp());
+void main() {
+  runApp(XylophoneApp());
+}
 
 class XylophoneApp extends StatelessWidget {
+  final player = AudioCache();
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Xylophone',
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Xylophone'),
-        ),
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              _xButton(Colors.red, 1),
+              _xButton(Colors.orange, 2),
+              _xButton(Colors.yellow, 3),
+              _xButton(Colors.green, 4),
+              _xButton(Colors.teal, 5),
+              _xButton(Colors.blue, 6),
+              _xButton(Colors.purple, 7),
+            ],
+          ),
         ),
+      ),
+    );
+  }
+
+  _xButton(Color noteColor, int noteNumber) {
+    return Expanded(
+      child: FlatButton(
+        color: noteColor,
+        onPressed: () {
+          player.play('note$noteNumber.wav');
+        },
       ),
     );
   }
